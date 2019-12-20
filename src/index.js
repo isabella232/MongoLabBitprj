a2be27861cb384babe6232ec6ffddb953d88898e
Keith Choung <kjchoung@ucdavis.edu>
2019-12-20 14:05:55 -0800

    deleted unnecessary lines

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 
 const express = require('express')
-require('./db/mongoose')
 
 const Volunteer = require('./models/volunteers')
-const volunteerRouter = require('./routers/volunteer')
+const volunteerRouter = require('./routers/volunteers')
 const adminRouter = require('./routers/admin')
 const app = express()
 const config = require('./config') // TODO: check
@@ -19,42 +18,3 @@ app.use(adminRouter)
 app.listen(port, () => {
     console.log('Server is up on port', port);
 })
-
-// // TODO: REST APIs down below
-// // Add Volunteer
-// app.post('/volunteers', (req, res) =>{
-//     const volunteer = new Volunteer(req.body)
-    
-//     volunteer.save().then((volunteer) => {
-//         res.status(201).send(volunteer)
-
-//     }).catch((e) => {
-//         res.status(400).send(e)
-//     })
-// })
-
-// // Read ALL volunteers
-// app.get('/volunteers', (req, res) => {
-//     Volunteer.find({}).then((volunteer) => {
-//         res.send(volunteer)
-//     }).catch((e) => {
-//         res.status(500).send()
-//     })
-// })
-
-// // TODO: Read ALL volunteers by First Name
-// app.get('/volunteers/:firstName', (req, res) => {
-//     const firstName = req.params
-//     console.log(firstName)
-
-//     Volunteer.find(firstName).then((volunteer) => {
-//         res.status(200).send(volunteer)
-//     }).catch((e) => {
-//         res.status(500).send()
-//     })
-// })
-
-// // TODO: Delete Volunteer
-
-
-// // TODO: Update Volunteer
