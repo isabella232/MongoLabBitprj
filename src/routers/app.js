1c46897a5d8bcb56919518e2d31815605d97d9d5
Keith Choung <kjchoung@ucdavis.edu>
2019-12-23 14:08:21 -0800

    refactored static to dynamic, re-did titles for cards

--- src/routers/app.js
+++ src/routers/app.js
@@ -2,15 +2,26 @@ const path = require('path')
 const express = require('express');
 require('../db/mongoose')
 const Event = require('../models/events')
+const hbs = require('hbs')
 const router = express();
 
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ]
 
+const bodyParser = require('body-parser')
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
 const publicDirectoryPath = path.join(__dirname, '../../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 router.set('view engine', 'hbs')
+router.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Set up static directory to serve
 router.use(express.static(publicDirectoryPath))
 
 router.get('', (req, res) => {
@@ -22,18 +33,20 @@ router.get('/turkey-trot', async (req, res) => {
 
 	try {
 		const event = await Event.findOne({eventName:"Turkey Trot"})
+		console.log(event);
 		res.render('turkey-trot', {
 			eventName: event.eventName,
 			coordinator: event.coordinator,
-			// date: event.date,
+			// // date: event.date,
 			month: monthNames[Number(event.date.slice(5, 7)) - 1].split("").join(" ").toUpperCase(),
 			day: event.date.slice(8),
 			time: event.time,
 			location: event.location,
-			description: event.description
+			description: event.description,
+			attendeeCount: event.attendeeCount
 		})
 	} catch(e) {
-		res.status(500).send()
+		res.status(500).send(e)
 	}
 })
 
@@ -45,12 +58,13 @@ router.get('/coastal-cleanup', async (req, res) => {
 		res.render('coastal-cleanup', {
 			eventName: event.eventName,
 			coordinator: event.coordinator,
-			// date: event.date,
+			// // date: event.date,
 			month: monthNames[Number(event.date.slice(5, 7)) - 1].split("").join(" ").toUpperCase(),
 			day: event.date.slice(8),
 			time: event.time,
 			location: event.location,
-			description: event.description
+			description: event.description,
+			attendeeCount: event.attendeeCount
 		})
 	} catch(e) {
 		res.status(500).send()
@@ -74,9 +88,16 @@ router.get('/contactus', (req,res) => {
 	router.render('contactus')
 })
 
-router.get('/events', (req,res) => { 
-	router.render('events')
-
+router.get('/events', async (req, res) => { 
+	try { 
+		const events = await Event.find({})
+		// console.log(events)
+		res.render('events', {
+			events: events
+		})
+	} catch (e) { 
+		res.status(600).send()
+	}
 })
 
 router.get('*', (req, res) => { 
